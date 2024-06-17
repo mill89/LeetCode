2269a7dba295c369d5ab023cde6908c7f7776532
Milan Nasrytinov <n.milan1989@gmail.com>
2024-06-17 22:41:14 +0600

    add 1672

--- richest_customer_1672.py
+++ richest_customer_1672.py
@@ -1,2 +1,7 @@
-def maximum_wealth(self, accounts: list[list[int]]) -> int:
-    pass
+def maximum_wealth(accounts: list[list[int]]) -> int:
+    return max(sum(n) for n in accounts)
+
+
+if __name__ == '__main__':
+    print(maximum_wealth([[1, 2, 3], [3, 2, 1]]))  # 6
+    print(maximum_wealth([[1, 5], [7, 3], [3, 5]]))  # 10
