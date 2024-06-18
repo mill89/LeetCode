0bc36562faeffbc3502c954d5b5909af8834ef75
Milan Nasrytdinov <n.milan1989@gmail.com>
2024-06-18 15:10:43 +0600

    add files 1342.py

---
+++ number_of_steps_1342.py
@@ -0,0 +1,37 @@
+def number_of_steps(num: int) -> int:
+    steps = 0
+    while num != 0:
+        num = num // 2 if num % 2 == 0 else num - 1
+        steps += 1
+
+    return steps
+
+
+if __name__ == '__main__':
+    print(number_of_steps(123))
+    '''
+    Example 1:
+
+Input: num = 14
+Output: 6
+Explanation: 
+Step 1) 14 is even; divide by 2 and obtain 7. 
+Step 2) 7 is odd; subtract 1 and obtain 6.
+Step 3) 6 is even; divide by 2 and obtain 3. 
+Step 4) 3 is odd; subtract 1 and obtain 2. 
+Step 5) 2 is even; divide by 2 and obtain 1. 
+Step 6) 1 is odd; subtract 1 and obtain 0.
+Example 2:
+
+Input: num = 8
+Output: 4
+Explanation: 
+Step 1) 8 is even; divide by 2 and obtain 4. 
+Step 2) 4 is even; divide by 2 and obtain 2. 
+Step 3) 2 is even; divide by 2 and obtain 1. 
+Step 4) 1 is odd; subtract 1 and obtain 0.
+Example 3:
+
+Input: num = 123
+Output: 12
+    '''
